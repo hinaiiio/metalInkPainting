03e187b4c20c43d1cc4d26c6dcea24372029f6ad
Hina <bob.piyo.1995@gmail.com>
2018-08-05 00:20:25 +0900

    Initial Commit

---
+++ MetalSandbox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MetalSandbox
+//
+//  Created by Hina on 2018/08/05.
+//  Copyright © 2018 Hina. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
